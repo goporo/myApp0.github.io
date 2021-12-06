c29c8c46dc18034824b078b8eaec19dc14e2623d
nguyen <goporolo@email.com>
2021-12-07 01:20:54 +0700

    Updates

---
+++ precache-manifest.127abd28168eb98ae1de9941d8599fa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d5bfe0069800e40b01be3b5e092da1",
+    "url": "/myApp0/index.html"
+  },
+  {
+    "revision": "1768d2f52732c855d41d",
+    "url": "/myApp0/static/css/main.e1a1814c.chunk.css"
+  },
+  {
+    "revision": "50c8bf2594658b614cab",
+    "url": "/myApp0/static/js/2.25f6f941.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/myApp0/static/js/2.25f6f941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1768d2f52732c855d41d",
+    "url": "/myApp0/static/js/main.ae2324b3.chunk.js"
+  },
+  {
+    "revision": "046fe3684ee52f0f7a74",
+    "url": "/myApp0/static/js/runtime-main.ff776b76.js"
+  }
+]);
